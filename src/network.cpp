33e436e330ab9a9a8a6a212d0aeb6727fc757d97
Denis Davydov <denidav@yandex.ru>
2018-05-11 15:23:28 +0300

    The function "sendPing" and "send" is added.

--- src/network.cpp
+++ src/network.cpp
@@ -1,6 +1,28 @@
 #include "network.h"
 
-network::network()
+class Network
 {
-
+    Network()
+    {
+        socket = new QUdpSocket(this);
+        socket->bind(QHostAddress::AnyIPv6, 6552);
+        connect(socket, SIGNAL(readyRead()), SLOT(read()));
+    }
+    void sendPing(QHostAddress ip)
+    {
+        json ping;
+        ping["action"] = "ping";
+        QString str = ping.dump();
+        send(str, ip);
+    }
+    void send(QString str, QHostAddress ip)
+    {
+        QByteArray data;
+        QDataStream out(&data, QIODevice::WriteOnly);
+        out << qint64(0);
+        out << str;
+        out.device()->seek(qint64(0));
+        out << qint64(data.size() - sizeof(qint64));
+        socket->writeDatagram(data, QHostAddress(ip), 6552);
+    }
 }
