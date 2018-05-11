69e3a70d7b6043165799accf8c694513f3b1754e
Denis Davydov <denidav@yandex.ru>
2018-05-11 15:27:07 +0300

    The Network class header file has been updated.

--- src/network.h
+++ src/network.h
@@ -1,11 +1,16 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include "lib/json/json.hpp"
+#include <QUdpSocket>
+using json = nlohmann::json;
 
 class network
 {
-public:
-    network();
+    public:
+        send(QString str, QHostAddress ip);
+    private:
+        sendPing(QHostAddress ip);
 };
 
 #endif // NETWORK_H
